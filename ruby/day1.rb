7598a9eb84aa7332e775cadf87728b0b6ccf2f3b
Thomas Solecki <thomasjsolecki@gmail.com>
2024-08-21 08:00:40 -0700

    [ruby] Add day 1 ruby practice

---
+++ ruby/day1.rb
@@ -0,0 +1,11 @@
+# 1.)
+puts 'Hello, world.'
+
+# 2.)
+index_of_ruby = 'Hello, world.'.index('world')
+puts index_of_ruby
+
+# 3.)
+10.times { puts 'thomas' }
+
+(1..10).each { |i| puts "this is sentence number #{i}" }
